f1b8c9ec104489c4832fc6771a6ee94e8dc5d1fe
Cory LaFlamme <me@corylaflamme.com>
2016-05-12 16:10:46 -0400

    Removed minimist - arg parsing isn't complex enough to warrant it.

--- cli.js
+++ cli.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 
-const argv = require('minimist');
 const fetch = require('./fetch');
-const args = argv(process.argv.slice(2));
+const args = process.argv.slice(2);
 
 function error(text) {
   process.stderr.write('\x1B[31m' + text);
 }
 
-if (args._.length < 1) {
+if (args.length < 1) {
   error('No URL specified.');
 } else {
-  fetch(args._[0], (err, iconUrl) => {
+  fetch(args[0], (err, iconUrl) => {
     process.stdout.write(iconUrl || '');
   });
 }
